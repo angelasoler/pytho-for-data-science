e0e3190ed0879777d054eb05bbfa152a490003d9
angelasoler <manzanaembicicleta@gmail.com>
2024-11-25 11:55:04 -0300

    adjust

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,13 +1,13 @@
 
 def all_thing_is_obj(object: any) -> int:
     num_obj = [int, bool, float]
-    type_name = type(object).__name__
+    type_name = type(object).__name__.capitalize()
     type_def = type(object)
 
     if isinstance(object, str):
-        print(f'{object} is in the kitchen: {type_def}')
+        print(f'{object} is in the kitchen : {type_def}')
     elif type(object) in num_obj:
         print("Type not found")
     else:
-        print(f'{type_name}: {type_def}')
+        print(f'{type_name} : {type_def}')
     return 42
