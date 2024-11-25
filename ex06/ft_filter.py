04b289207984f3b8c001e5d719fdb7c298e6a38a
angelasoler <manzanaembicicleta@gmail.com>
2024-11-25 11:51:47 -0300

    adjust

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,12 +1,7 @@
 def ft_filter(func, iterable):
-    """
+    """filter(function or None, iterable) --> filter object
 
-    Args:
-        func (function): return a boolean of the condition to filter
-        iterable (any): iterable to be filter
-
-    Returns:
-        list: return a new list with the object filtered by function
-    """
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
     ret = [item for item in iterable if func(item)]
     return ret
